a934378514c59f20e11881ac5de730fca961acbd
Nicholas Kullman <nick.kullman@gmail.com>
2017-02-20 23:23:38 -0800

    Add retrieval and update of the log

--- app/src/main/java/io/github/nkullman/glog/addNewEntryActivity.java
+++ app/src/main/java/io/github/nkullman/glog/addNewEntryActivity.java
@@ -1,10 +1,14 @@
 package io.github.nkullman.glog;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.gson.Gson;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,8 +18,21 @@ public class addNewEntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_entry);
+
+        // get gLog's shared preferences
+        SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
+
+        // get the ID of the log to which to assign the entry
+        Intent intent = getIntent();
+        String logId = intent.getStringExtra("log_id");
+
+        // retrieve the log
+        Gson gson = new Gson();
+        String json = mPrefs.getString(logId,"");
+        final gLog_Log currLog = gson.fromJson(json,gLog_Log.class);
 
         Button doneButton = (Button) findViewById(R.id.submitEntrybutton);
         assert doneButton != null;
@@ -28,8 +45,9 @@ public class addNewEntryActivity extends AppCompatActivity {
                 double or = Double.parseDouble((
                         (EditText)findViewById(R.id.odometerReading_editText)).getText().toString());
                 SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                Date date = null;
                 try {
-                    Date date = df.parse(((
+                    date = df.parse(((
                             (EditText)findViewById(R.id.date_editText)).getText().toString()));
                 } catch (ParseException e) {
                     e.printStackTrace();
@@ -45,25 +63,13 @@ public class addNewEntryActivity extends AppCompatActivity {
                 String notes = ((EditText)findViewById(R.id.demoNote_editText)).getText().toString();
                 if (notes.equals("\"While taking Grandma to get ice cream. Filled up with premium\"")) notes = "";
 
-                // todo this class (intent) should take a string indicating what log the entry belongs to
-                // then it should look it up and retrieve it from the sharedpreferences using the
-                // gson object
-                // todo the welcome screen should read from the sharedpreferences thing the names of
-                // all the logs (iterate over keys whose name starts with log_[...VEHICLEINFO_odometer]
-                // populate the dropdown list for logs to choose from based off the responses from this iterating
-                // if the list is empty, then disable the "add entry" button and just enable the add new log button
+                currLog.addEntry(new gLog_Entry(date,loc,fs,or,ppg,galsFilled,notes));
 
+                // todo update some stats, show screen w summary statistics
+                // "Since last fill! -- mpg, mi driven,..."
+                // have an OK button at the bottom of that screen
+                // hitting that takes you to the home page
             }
         });
     }
-
-
-
-    // if none, add the new entry to the log we're working with
-    // update some stats or something
-    // go to the screen with summary statistics:
-    // "Since last fill! -- mpg, mi driven,..."
-    // have an OK button at the bottom of that screen
-    // hitting that takes you to the home page
-    // where todo going to add a "view data" button or something
 }
