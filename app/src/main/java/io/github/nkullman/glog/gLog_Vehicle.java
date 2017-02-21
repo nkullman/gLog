cd47b9eaec3029052094cc264086022171d7403d
Nicholas Kullman <nick.kullman@gmail.com>
2017-02-20 19:22:17 -0800

    Add odometer reading field, getters, setters, and comments

--- app/src/main/java/io/github/nkullman/glog/gLog_Vehicle.java
+++ app/src/main/java/io/github/nkullman/glog/gLog_Vehicle.java
@@ -8,20 +8,67 @@ public class gLog_Vehicle {
     private String make;
     private String model;
     private int year;
+    private double odometer_reading;
 
-    public gLog_Vehicle(int year, String make, String model){
+    /**
+     * Construct a new vehicle of the year given make, model, and year with the
+     * given odometer reading.
+     */
+    public gLog_Vehicle(int year, String make, String model, double odometer_reading) {
         this.year = year;
         this.make = make;
         this.model = model;
+        this.odometer_reading = odometer_reading;
     }
 
-    public gLog_Vehicle(){
+    /**
+     * Default constructor makes a brand new 2015 Subaru Forester
+     */
+    public gLog_Vehicle() {
         this.year = 2015;
         this.make = "Subaru";
         this.model = "Forester";
+        this.odometer_reading = 0;
     }
 
+    /* Setters */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setOdometer_reading(double odometer_reading) {
+        this.odometer_reading = odometer_reading;
+    }
+
+    /* Getters */
+    public double getOdometer_reading() {
+        return this.odometer_reading;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    /**
+     * Clone the vehicle
+     */
     public gLog_Vehicle clone() {
-        return new gLog_Vehicle(this.year,this.make,this.model);
+        return new gLog_Vehicle(this.year, this.make, this.model, this.odometer_reading);
     }
 }
